802f75892753f7239ac5ecd1bef57b051f3df314
kottevijay <kottevijay@Gmail.com>
2024-12-20 19:54:55 +0530

    Added Java

---
+++ FirstJava/src/firsttest/sample1/FirstSample.java
@@ -0,0 +1,16 @@
+package firsttest.sample1;
+
+public class FirstSample {
+
+	int a=10;
+	
+	public void m1()
+	{
+	System.out.println(a);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("VJ");
+	}
+
+}
